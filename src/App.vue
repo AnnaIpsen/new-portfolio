d2dd8af1ab254671dfea51d3dcb58b64ff40441b
Anna-1902 <annaipsen19@gmail.com>
2023-01-27 16:49:34 +0100

    setup + home view

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,60 @@
 <template>
-
+  <nav>
+    <router-link to="/"><img src="./assets/logo.png"></router-link>
+    <router-link to="/about">About me</router-link>
+    <router-link to="/projects">Projects</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-
-}
-</script>
-
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+body {
+  background-color: #FBF6EF;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #98A589;
+  margin: 2rem 4rem;
+}
+
+h1, h2, h3, h4, nav {
+  font-family: "Playfair Display", sans-serif;
+  text-transform: uppercase;
+}
+h1, h2 {
+  font-weight: 400;
+  letter-spacing: .3rem;
+}
+h1 {font-size: 30px;}
+h2 {font-size: 16px;}
+p {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 12px;
+}
+
+nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 4rem;
+}
+
+nav img {
+  height: 2rem;
+}
+
+nav a {
+  font-weight: 500;
+  color: #98A589;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+
+nav a:nth-child(n+2).router-link-exact-active {
+  border-bottom: #98A589 solid 1px;
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <h1>Anna Ipsen</h1>
+    <h2>creative frontend developer</h2>
+    <img id="flowers" src="../assets/blomsterPng.png">
+    <hr/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5rem;
+}
+.home h1, .home h2 {
+  margin: .2rem 0;
+}
+#flowers {
+  width: 50vw;
+}
+hr {
+  width: 100%;
+  margin-top: -1px;
+  border: .5px solid #98A589;
+}
+</style>
