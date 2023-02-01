13a149b9bec4a836b9cc4bf22932604891033632
Anna-1902 <annaipsen19@gmail.com>
2023-02-01 19:50:55 +0100

    project view with nav

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div>
+    <section class="projectsNav">
+      <router-link to="/projects/work">Projects</router-link>
+      <router-link to="/projects/hobby">Hobby projects</router-link>
+    </section>
+    <router-view/>
+  </div>
 </template>
 
 <script>
@@ -9,5 +15,21 @@ export default {
 </script>
 
 <style scoped>
+.projectsNav {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+.projectsNav a {
+  font-weight: 500;
+  color: #98A589;
+  text-decoration: none;
+  text-transform: uppercase;
+  border-bottom: #98A589 solid 1px;
+  padding: .5rem 4rem;
+}
 
+.projectsNav a.router-link-exact-active {
+  border-bottom: #98A589 solid 2px;
+}
 </style>
