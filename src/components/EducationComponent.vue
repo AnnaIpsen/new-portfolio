f7749fb6e35a7ef88332722d04a6d7b26a52f7bc
Anna-1902 <annaipsen19@gmail.com>
2023-03-01 19:25:18 +0100

    CV education component + content

---
+++ src/components/EducationComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="education">
+    <section>
+      <p class="cv_heading">{{name}}</p>
+      <p class="dateInterval">{{when}}</p>
+    </section>
+    <p class="cv_heading">{{where}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EducationComponent",
+  props: {
+    name: String,
+    where: String,
+    when: String
+  }
+}
+</script>
+
+<style scoped>
+.education {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  max-width: 25rem;
+  margin-bottom: 1rem;
+}
+.cv_heading {
+  font-size: 14px;
+  font-family: 'Montserrat', sans-serif;
+  color: #2E2E2E;
+  margin-bottom: 0;
+}
+.dateInterval {margin: 0; font-style: italic}
+
+</style>
--- src/views/CvView.vue
+++ src/views/CvView.vue
@@ -1,13 +1,82 @@
 <template>
-<section></section>
+  <div class="cv">
+    <section class="headings">
+      <h1>CV</h1>
+    </section>
+    <section class="cv">
+      <img src="../assets/Anna-Ipsen-cv.jpg">
+      <div class="education">
+        <h2>Education</h2>
+        <education-component
+            v-for="(education, index) in educations"
+            :key="index"
+            :name="education.name"
+            :when="education.when"
+            :where="education.where"
+        />
+      </div>
+      <div class="tech">
+        <h2>Technologies</h2>
+        <div class="techWrapper">
+          <p
+              v-for="(tech, index) in techs"
+              :key ="index"
+          >
+            {{ tech }}
+          </p>
+        </div>
+      </div>
+    </section>
+
+  </div>
 </template>
 
 <script>
+import educationComponent from "@/components/EducationComponent";
+import cv from "../content/cv_content.json"
 export default {
-  name: "CvView"
+  name: "CvView",
+  components: {educationComponent},
+  data () {
+    return {
+      educations: cv.education,
+      works: cv.work,
+      volunteerings: cv.volunteering,
+      skills: cv.skills,
+      languages: cv.language,
+      techs: cv.tech,
+    }
+  }
 }
 </script>
 
 <style scoped>
+.cv {
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+}
+.headings {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.headings h1 {
+  margin: .2rem 0;
+}
+img {
+  width: 20rem;
+}
+
+.techWrapper {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 25rem;
+}
+.techWrapper p {width: 8rem}
 
 </style>
