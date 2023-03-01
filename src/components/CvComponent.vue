9731d77d0aa0ed762a073ddeb9e1eb157a3cec7e
Anna-1902 <annaipsen19@gmail.com>
2023-03-01 20:22:41 +0100

    CV work, volunteering + style

--- src/components/CvComponent.vue
+++ src/components/CvComponent.vue
@@ -2,6 +2,7 @@
   <div class="education">
     <section>
       <p class="cv_heading">{{name}}</p>
+      <p v-if="company" class="company"> {{company}}</p>
       <p class="dateInterval">{{when}}</p>
     </section>
     <p class="cv_heading">{{where}}</p>
@@ -14,7 +15,8 @@ export default {
   props: {
     name: String,
     where: String,
-    when: String
+    when: String,
+    company: String
   }
 }
 </script>
@@ -24,15 +26,15 @@ export default {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  max-width: 25rem;
   margin-bottom: 1rem;
 }
 .cv_heading {
   font-size: 14px;
   font-family: 'Montserrat', sans-serif;
   color: #2E2E2E;
-  margin-bottom: 0;
+  margin-bottom: 4px;
 }
+.company {margin: 0 0 4px 0;}
 .dateInterval {margin: 0; font-style: italic}
 
 </style>
--- src/views/CvView.vue
+++ src/views/CvView.vue
@@ -3,40 +3,68 @@
     <section class="headings">
       <h1>CV</h1>
     </section>
-    <section class="cv">
-      <img src="../assets/Anna-Ipsen-cv.jpg">
-      <div class="education">
-        <h2>Education</h2>
-        <education-component
-            v-for="(education, index) in educations"
-            :key="index"
-            :name="education.name"
-            :when="education.when"
-            :where="education.where"
-        />
-      </div>
-      <div class="tech">
-        <h2>Technologies</h2>
-        <div class="techWrapper">
-          <p
-              v-for="(tech, index) in techs"
-              :key ="index"
-          >
-            {{ tech }}
-          </p>
+    <section class="cvWrapper">
+      <section class="row">
+        <div class="education">
+          <h2>Education</h2>
+          <cv-component
+              v-for="(education, index) in educations"
+              :key="index"
+              :name="education.name"
+              :when="education.when"
+              :where="education.where"
+          />
         </div>
-      </div>
+        <div class="work">
+          <h2>Working History</h2>
+          <cv-component
+              v-for="(work, index) in works"
+              :key="index"
+              :name="work.name"
+              :when="work.when"
+              :where="work.where"
+              :company="work.company"
+          />
+        </div>
+        <div class="volunteering">
+          <h2>Volunteering</h2>
+          <cv-component
+              v-for="(job, index) in volunteerings"
+              :key="index"
+              :name="job.name"
+              :when="job.when"
+              :where="job.where"
+          />
+        </div>
+      </section>
+      <section class="row">
+        <img src="../assets/Anna-Ipsen-cv.jpg">
+        <div class="tech">
+          <h2>Technologies</h2>
+          <div class="techWrapper">
+            <p
+                v-for="(tech, index) in techs"
+                :key ="index"
+            >
+              {{ tech }}
+            </p>
+          </div>
+        </div>
+
+      </section>
+
+
     </section>
 
   </div>
 </template>
 
 <script>
-import educationComponent from "@/components/EducationComponent";
 import cv from "../content/cv_content.json"
+import CvComponent from "@/components/CvComponent";
 export default {
   name: "CvView",
-  components: {educationComponent},
+  components: {CvComponent},
   data () {
     return {
       educations: cv.education,
@@ -54,8 +82,23 @@ export default {
 .cv {
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
   gap: 4rem;
 }
+
+.cvWrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+}
+.row {
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+}
+.education, .volunteering, .work {width: 25rem}
+.tech, img {width: 25rem}
 .headings {
   display: flex;
   flex-direction: column;
@@ -66,16 +109,12 @@ export default {
 .headings h1 {
   margin: .2rem 0;
 }
-img {
-  width: 20rem;
-}
 
 .techWrapper {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-between;
-  width: 25rem;
 }
 .techWrapper p {width: 8rem}
 
