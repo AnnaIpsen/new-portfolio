bd9dc018ead90e196f8faf5f2924058d05700b50
Anna-1902 <annaipsen19@gmail.com>
2023-03-18 13:56:38 +0100

    Update HobbyProjects.vue

--- src/components/HobbyProjects.vue
+++ src/components/HobbyProjects.vue
@@ -2,36 +2,36 @@
   <section class="hobby">
     <section class="headings">
       <h1>Hobby projects</h1>
-      <h2>photography, adobe illustrator, crochet & baking</h2>
+      <h3>photography, adobe illustrator, crochet & baking</h3>
     </section>
     <div class="collage">
       <section class="column">
-        <img src="../assets/project-images/moontart.jpg" >
+        <img src="../assets/project-images/moontart.jpg">
         <img src="../assets/project-images/nz.jpg" >
-        <img src="../assets/project-images/citron.png" class="reveal">
-        <img src="../assets/project-images/hue.jpg" class="reveal">
-        <img src="../assets/project-images/water.jpg" class="reveal">
-        <img src="../assets/project-images/gul-kage.jpg" class="reveal">
-        <img src="../assets/project-images/paris.jpg" class="reveal">
+        <img src="../assets/project-images/citron.png">
+        <img src="../assets/project-images/hue.jpg" >
+        <img src="../assets/project-images/water.jpg" >
+        <img src="../assets/project-images/gul-kage.jpg">
+        <img src="../assets/project-images/paris.jpg">
 
       </section>
       <section class="column">
         <img src="../assets/project-images/hortensia.jpg" >
         <img src="../assets/project-images/kage.png" >
-        <img src="../assets/project-images/nissehue.jpg" class="reveal">
-        <img src="../assets/project-images/vin.png" class="reveal">
-        <img src="../assets/project-images/minitaerte.jpg" class="reveal">
-        <img src="../assets/project-images/croissant.png" class="reveal">
+        <img src="../assets/project-images/nissehue.jpg" >
+        <img src="../assets/project-images/vin.png">
+        <img src="../assets/project-images/minitaerte.jpg" >
+        <img src="../assets/project-images/croissant.png">
 
       </section>
       <section class="column">
         <img src="../assets/project-images/bryllupskage.jpg" >
         <img src="../assets/project-images/grapes.jpg" >
-        <img src="../assets/project-images/generationer.png" class="reveal">
-        <img src="../assets/project-images/halsedisse.jpg" class="reveal">
-        <img src="../assets/project-images/vejle.jpg" class="reveal">
-        <img src="../assets/project-images/pildskadde.png" class="reveal">
-        <img src="../assets/project-images/taerte.jpg" class="reveal">
+        <img src="../assets/project-images/generationer.png" >
+        <img src="../assets/project-images/halsedisse.jpg">
+        <img src="../assets/project-images/vejle.jpg" >
+        <img src="../assets/project-images/pildskadde.png" >
+        <img src="../assets/project-images/taerte.jpg">
       </section>
     </div>
   </section>
@@ -40,26 +40,7 @@
 <script>
 export default {
   name: 'hobbyProjects',
-  methods: {
-    reveal () {
-      const reveals = document.querySelectorAll(".reveal");
-
-      for (let i = 0; i < reveals.length; i++) {
-        let windowHeight = window.innerHeight;
-        let elementTop = reveals[i].getBoundingClientRect().top;
-        let elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-          reveals[i].classList.add("active");
-        } else {
-          reveals[i].classList.remove("active");
-        }
-      }
-    }
-  },
-  mounted () {
-    window.addEventListener('scroll', this.reveal);
-  }
+  methods: {}
 }
 </script>
 
@@ -92,28 +73,8 @@ export default {
   margin-top: 2rem;
   width: 100%;
   border-radius: 20px;
+  transition: all 0.4s ease-in-out 0s;
 }
 
-.reveal{
-  position: relative;
-  transform: translateY(150px);
-  opacity: 0;
-  transition: ease-in .5s;
-}
-.reveal.active{
-  transform: translateY(0);
-  opacity: 1;
-  animation: fade-buttom .5s ease-in;
-}
-@keyframes fade-bottom {
-  0% {
-    transform: translateY(50px);
-    opacity: 0;
-  }
-  100% {
-    transform: translateY(0);
-    opacity: 1;
-  }
-}
 
 </style>
